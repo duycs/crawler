669c2f26f6963eaad6094da5ee291ca8513ab95f
thang-nm <nm.thang@outlook.com>
2023-08-03 09:14:58 +0700

    sqlite sample code

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const puppeteer = require("puppeteer");
+const sqlite3 = require("sqlite3");
+
+const db = new sqlite3.Database("./db.sqlite");
 
 (async function main() {
   const browser = await puppeteer.launch({
@@ -9,22 +12,31 @@ const puppeteer = require("puppeteer");
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15"
   );
   await page.goto("https://dstock.vndirect.com.vn/d-rating-chuyen-sau/HPG");
-  await new Promise((resolve) => setTimeout(resolve, 5000));
   await page.waitForSelector(".drating-box__main", {
     visible: true,
     timeout: 5000,
   });
-  
-  const box = await page.evaluate(
-    () => document.querySelector(".drating-box__main").innerHTML
+
+  const data = await page.$$eval(".drating-box__main table tr td", (tds) =>
+    tds.map((td) => {
+      return td.innerText;
+    })
   );
 
-  console.log(box);
+  db.serialize(() => {
+    db.prepare('INSERT INTO "data" ("value") VALUES (?)')
+      .run(JSON.stringify(data))
+      .finalize();
+  });
 
-  const data = await page.$$eval('.drating-box__main table tr td', tds => tds.map((td) => {
-    return td.innerText;
-  }));
-  
+  // wait for db to be closed before exit
+  await new Promise((resolve, reject) => {
+    db.close((err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+
   console.log(data);
 
   process.exit(0);
